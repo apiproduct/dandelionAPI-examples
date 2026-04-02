a379ed6f035b6f5fc142896cd68ef95e3a38da97
apiproduct <api_product@riafinancial.com>
2026-04-02 16:12:52 +0200

    Create good.js

---
+++ node/error-handling/missing-subscription-key/good.js
@@ -0,0 +1,23 @@
+// Example: Missing subscription key
+
+const axios = require('axios');
+
+let config = {
+  method: 'get',
+  url: 'https://mt-staging.dandelionpayments.com/Network/countries',
+  headers: {
+    'Accept-Language': 'en-US',
+    'Host': 'mt-staging.dandelionpayments.com',
+    'Accept': 'application/json',
+    'Ocp-Apim-Subscription-Key': '<SUBSCRIPTION_KEY>',
+    'Authorization': 'Bearer <ACCESS_TOKEN>'
+  }
+};
+
+axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
